b257dbef2ec76ac00b3d4b80bd7863c34808dc73
AaronKurian <aaronkurianabraham@gmail.com>
2024-11-24 10:52:53 +0530

    Second Commit

--- app/components/heading.tsx
+++ app/components/heading.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { useState } from "react";
 
-export default function Heading() 
-{
-    const [state, setState] = useState{"helllo"};
+export default function Heading() {
+    const [state, setState] = useState("hello"); // Corrected useState syntax
     return <h1>{state}</h1>;
 }
